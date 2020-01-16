7e6a3ef1a4ca8916333e96f5b0db286702cb0de8
ss <chandu220199@gmail.com>
2020-01-16 10:50:18 +0530

    Changes are done for Ticket:      Create Hello World Program     Define Human
          Changes are :
    	modified:   Hello.c
    	new file:   Human.c
    	new file:   Human.h

--- Code/Hello.c
+++ Code/Hello.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 int main(){
-    printf("Hello World");
+  int doSomething=100;  
+  printf("Hello World");
+  printf("Welcome" );
 
 }
---
+++ Code/Human.h
@@ -0,0 +1,7 @@
+typedef struct human
+_type{
+   int hid;
+   char name[200];
+}Human
+;
+
